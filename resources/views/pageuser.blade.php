ee15d45a57b1d201916b71331efb10f34602bc34
khoudia96gueye <61237589+khoudia96gueye@users.noreply.github.com>
2023-10-24 21:14:48 +0000

    Add files via upload

---
+++ resources/views/pageuser.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MAYAN STORE</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="dist/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+</head>
+<body>
+    
+    <div class="container-fluid">
+        <div class="row flex-nowrap">
+            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-light ">
+                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-5 text-white min-vh-100">
+                    <a href="#" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                        <h1><i class="bi bi-person-circle " style="color: blue;"></i></h1>
+                    </a>
+                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                        <li>
+                            <a href="#" class="nav-link align-middle px-0">
+                                <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Dashbord</span> 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0 align-middle">
+                            <i class="bi bi-person-circle"></i> <span class="ms-1 d-none d-sm-inline">Users</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0 align-middle">
+                                <i class="bi bi-gear-fill"></i> <span class="ms-1 d-none d-sm-inline">setting</span> 
+                            </a>
+                        </li>
+                        
+                    
+                </div>
+            </div>
+            <div class=" container">
+            <header>
+                <nav class="navbar navbar-expand-lg " style=" background-color:whitesmoke;">
+                    <div class="container-fluid">
+                        
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="true" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <a href="#" class="nav-link px-0 align-middle">
+                            <i class="bi bi-gear-fill"></i> <span class="ms-5  d-sm-inline h2">setting</span> 
+                        </a>
+                    </div>
+                </nav>
+            </header>
+    
+                <div class="col-md-2">
+                  <i class="bi bi-envelope h1" style="width:200px; height:200px;"></i>   
+                </div>
+             
+
+                <form action="">
+                    
+                    <input type="text" name="email" placeholder="choose your logo" 
+                    style="width:300px;" > 
+                    <button style=" background-color:blue; color:white;">browser</button> <br><br>
+                
+                    <input type="text" name="password"  placeholder="copyright 2006 2018"  style="width:380px; "> <br> <br> 
+        
+                    <input type="submit" name="submit" style=" background-color:blue; color:white;" value="save">
+                </form>
+            </div>
+        </div>
+        </div>
+    </div>
+    
+
+    <!-- Scripts Bootstrap -->
+    <<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+    crossorigin="anonymous"></script>
+</body>
+</html> 
+
+
